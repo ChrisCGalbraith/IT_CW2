5bf79362457c1dcd67b98ebe820d67b898dc0659
ChrisCGalbraith <chriscgalbraith@gmai.com>
2021-01-17 12:46:59 +0000

    Updated routes for rooms, speakers & bookings

--- src/App.js
+++ src/App.js
@@ -6,31 +6,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
+import Home from "./components/home.component";
 
 import logo from "./logo.png";
 
 class App extends Component {
   render() {
-    return (
+   return (
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            <a class="navbar-brand" href="localhost:3000" target="_blank">
+              <img src={logo} width="40" height="40" alt="Home"/>
             </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <Link to="/home" className="navbar-brand">Home</Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
+                  <Link to="/speakers" className="nav-link">Speakers</Link>
                 </li>
                 <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
+                  <Link to="/rooms" className="nav-link">Rooms</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/bookings" className="nav-link">Bookings</Link>
                 </li>
               </ul>
             </div>
           </nav>
           <br/>
+          <Route path="/home" exact component={Home} />
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
