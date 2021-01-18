d789bcdaf27dfd50931bc30984ff5bea8455fe3e
ChrisCGalbraith <chriscgalbraith@gmai.com>
2021-01-18 16:52:30 +0000

    Push background image code for Home

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -2,4 +2,35 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class Home extends Component {
+    
+        constructor(props){
+            super(props);
+        }
+
+        render() {
+            return (
+                <div>
+                    <h3>Speakers</h3>
+                    <table className="table table-striped" style={{marginTop: 20 }}>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Subject</th>
+                                <th>Room</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           {/* { this.todoList() } */}
+                        </tbody>
+                    </table>
+                </div>
+            )
+        }
+
+        // todoList() {
+        //     return this.state.todos.map(function(currentTodo, i){
+        //         return <Todo todo={currentTodo} key={i} />;
+        //     })
+        // }
 };
