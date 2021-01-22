7168cc894e18b5cc5544387fc76147f1f751ffec
ChrisCGalbraith <chriscgalbraith@gmai.com>
2021-01-22 13:41:34 +0000

    Created navbar component to abstract it

--- src/App.js
+++ src/App.js
@@ -1,60 +1,40 @@
+/* 
+    Code initially generated from tutorial: https://codingthesmartway.com/the-mern-stack-tutorial-building-a-react-crud-application-from-start-to-finish-part-1/
+    and re-worked for the purposes of Internet Technologies Coursework 2.
+*/
+
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import './App.css';
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
 import Home from "./components/home.component";
+import Navbar from "./components/navbar.component";
 
 import logo from "./logo.png";
 
 class App extends Component {
+
   render() {
-   return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-            <a class="navbar-brand" href="localhost:3000" target="_blank">
-              <img src={logo} width="40" height="40" alt="Home"/>
-            </a>
-            <Link to="/home" className="navbar-brand">Home</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/speakers" className="nav-link">Speakers</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/rooms" className="nav-link">Rooms</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/bookings" className="nav-link">Bookings</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/home" exact component={Home} />
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
+    return (
+      <>
+        <div
+          className="landing-wrapper"
+          style={{
+            backgroundImage: "url(/background.png)",
+            backgroundPosition: '0% 0%',
+            width: '100vw',
+            height: '100vh',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+          }}
+        >
         </div>
-        <div className="container">
-          <div
-            className="landing-wrapper"
-            style={{
-              backgroundImage: "url(/background.png)",
-              backgroundPosition: 'center',
-              width: '100vw',
-              height: "100vh",
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat'
-            }}
-          >
-          </div>
-        </div>
-      </Router>
+        <Navbar />
+      </>
     );
   }
 }
---
+++ src/components/navbar.component.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CreateTodo from "./create-todo.component";
+import EditTodo from "./edit-todo.component";
+import TodosList from "./todos-list.component";
+import Home from "./home.component";
+import logo from "../logo.png";
+
+export default class Navbar extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+                        <a class="navbar-brand" href="localhost:3000" target="_blank">
+                            <img src={logo} width="40" height="40" alt="Home" />
+                        </a>
+                        <Link to="/home" className="navbar-brand">Home</Link>
+                        <div className="collpase navbar-collapse">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="navbar-item">
+                                    <Link to="/speakers" className="nav-link">Speakers</Link>
+                                </li>
+                                <li className="navbar-item">
+                                    <Link to="/rooms" className="nav-link">Rooms</Link>
+                                </li>
+                                <li className="navbar-item">
+                                    <Link to="/bookings" className="nav-link">Bookings</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <br />
+                    <Route path="/home" exact component={Home} />
+                    <Route path="/" exact component={TodosList} />
+                    <Route path="/edit/:id" component={EditTodo} />
+                    <Route path="/create" component={CreateTodo} />
+                </div>
+            </Router>
+        )
+    }
+}
+
